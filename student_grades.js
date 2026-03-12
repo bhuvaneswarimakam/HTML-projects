65b10aaf4642a7c1499125e829c2112185c7cb95
bhuvaneswarimakam <makambhuvana36@gmail.com>
2026-03-12 10:19:38 +0530

    Add files via upload

---
+++ student_grades.js
@@ -0,0 +1,9 @@
+console.log("To find student grades")
+let marks=95
+if(marks>=75){
+    console.log("Grade A");
+} else if(marks>=50){
+   console.log("Grade B"); 
+} else {
+   console.log("Grade c"); 
+}
