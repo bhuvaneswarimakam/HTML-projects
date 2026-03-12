a66014048ebed3fef6dda6e1af127003685b5db6
bhuvaneswarimakam <makambhuvana36@gmail.com>
2026-03-12 10:19:07 +0530

    Add files via upload

---
+++ number_sign.js
@@ -0,0 +1,9 @@
+console.log("To find the given number is positive,negative or Zero")
+let num=-2
+if(num>0){
+    console.log("positive number");
+} else if(num<0){
+   console.log("negative number");
+} else{
+    console.log("Zero"); 
+}
